f7b633bdcebe17ac687585a8781b3a55fad01bd7
Tharukie1996 <tharukiej@gmail.com>
2020-11-14 15:13:15 +0530

    Initial commit

---
+++ src/test/java/com/product/priceengine/PriceengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.priceengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
